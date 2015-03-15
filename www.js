04141638f7efc1ca45499fc3f72c26c6291393aa
Drew Walker <drew.walker@gmail.com>
2015-03-15 11:02:19 +1100

    Switched to jade templates for index page in preparation for Sign in With Twitter.

--- www.js
+++ www.js
@@ -3,14 +3,21 @@ var express = require('express'),
     app = express();
 
 app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 app.use(express.static(__dirname + '/public'));
 
 var twitter = require('./routes/twitter.js'),
     hue = require('./routes/hue.js');
+    //signIn = require('./routes/sign-in.js');
 
 app.use('/api/v1/twitter', twitter);
 app.use('/api/v1/hue', hue);
+//app.use('/api/v1/sign-in', signIn);
 
 app.listen(app.get('port'));
 
