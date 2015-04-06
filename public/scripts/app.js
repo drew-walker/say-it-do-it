3f9bf8d8330f7bc2b0f85d808c5808558232e803
Drew Walker <drew.walker@gmail.com>
2015-04-06 16:36:52 +1000

    Separated views and added start of settings page.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,10 @@
-angular.module('SayIt', ['ui.bootstrap', 'firebase', 'SIDI']);
+angular.module('SayIt', ['ui.bootstrap', 'firebase', 'SIDI', 'ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '/views/index.html'
+            })
+            .when('/settings', {
+                templateUrl: '/views/settings.html'
+            })
+    });
