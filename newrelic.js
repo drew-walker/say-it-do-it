38e494853eff36c84021841081965ff03189bd97
Drew Walker <drew.walker@gmail.com>
2015-04-11 09:33:09 +1000

    Added New Relic and increased size of login button.

---
+++ newrelic.js
@@ -0,0 +1,7 @@
+/* NOTE: The New Relic license key is set with the NEW_RELIC_LICENSE_KEY environment variable. */
+exports.config = {
+  app_name: ['SayItDoIt'],
+  logging: {
+    level: 'trace'
+  }
+};
--- run.js
+++ run.js
@@ -1,2 +1,7 @@
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+    console.log('New Relic agent started')
+    require('newrelic');
+}
+
 var www = require('./src/www.js');
 www.start(process.env.PORT || 5000);
